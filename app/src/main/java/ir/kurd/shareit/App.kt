c90e4a5f1fcb71a44cf329badf9348b3eb820bdf
Arvin <arvinflash@gmail.com>
2021-01-14 17:44:58 +0330

    multidex

--- app/src/main/java/ir/kurd/shareit/App.kt
+++ app/src/main/java/ir/kurd/shareit/App.kt
@@ -1,11 +1,12 @@
 package ir.kurd.shareit
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import ir.kurd.shareit.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App: Application() {
+class App: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
